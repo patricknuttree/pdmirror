3f9ba8a8a74236a58bf3f9ddccf9403a80e7a2b0
Patrick Nusbaum <patricknusbaum2016@gmail.com>
2021-06-02 15:40:04 -0400

    Database models are built and working

--- app/models/jurisdiction.py
+++ app/models/jurisdiction.py
@@ -2,11 +2,11 @@ from .db import db
 
 
 class Jurisdiction(db.Model):
-    __tablename__ = 'jurisdiction'
+    __tablename__ = 'jurisdictions'
 
     id = db.Column(db.Integer, primary_key=True)
     location = db.Column(db.String(100), nullable=False)
-    pd_id = db.Column(db.Integer, db.ForeignKey("pd.id"), nullable=False)
+    pd_id = db.Column(db.Integer, db.ForeignKey("pds.id"), nullable=False)
     pd = db.relationship("Pd", back_populates="jurisdiction")
 
     def to_dict(self):
--- app/models/pd.py
+++ app/models/pd.py
@@ -3,12 +3,12 @@ from .db import db
 
 
 class Pd(db.Model):
-    __tablename__ = 'pd'
+    __tablename__ = 'pds'
 
     id = db.Column(db.Integer, primary_key=True)
     department_name = db.Column(db.String(100), nullable=False)
-    jurisdictions = relationship("Jurisdiction", back_populates="pd")
-    reflections = relationship("Reflection", back_populates="pd")
+    jurisdictions = db.relationship("Jurisdiction", back_populates="pd")
+    reflections = db.relationship("Reflection", back_populates="pd")
 
     def to_dict(self):
         return {
