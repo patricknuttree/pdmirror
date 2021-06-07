882774874df8a2256d0719ddfce7d8644b619862
Patrick Nusbaum <patricknusbaum2016@gmail.com>
2021-06-07 16:27:21 -0400

    Added reflection seed data for demo user to pd.id.1

--- app/api/pd_routes.py
+++ app/api/pd_routes.py
@@ -2,13 +2,24 @@ from app.models.pd import Pd
 from flask import request
 from flask.blueprints import Blueprint
 from flask_login import login_required, current_user
-from app.models import db, Pd
+from app.models import db, Pd, Reflection, User
 
 pd_routes = Blueprint("pds", __name__)
 
 
+# GET ALL THE POLICE DEPARTMENTS
 @pd_routes.route('/')
 def pds():
     pds = Pd.query.all()
     # return "Hello World"
     return {"pds": [pd.to_dict() for pd in pds]}
+
+
+# GET ALL THE REFLECTIONS FOR A POLICE DEPARTMENT (api/pds/:id/reflections)
+@pd_routes.route('<int:id>/reflections')
+def pd_reflections(id):
+    reflections = Reflection.query(Reflection.pd_id == id).join(
+        User, User.id == Reflection.user_id).all()
+    return {
+        "reflections": [reflection.to_dict() for reflection in reflections]
+        }
--- react-app/src/components/singlePD/SinglePoliceDepartment.js
+++ react-app/src/components/singlePD/SinglePoliceDepartment.js
@@ -1,9 +1,32 @@
-import React from "react"
-
+import React, { useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { useParams } from "react-router"
+import { displayPds } from "../../store/pd";
 
 const SinglePoliceDepartment = () => {
+    const { pdId } = useParams();
+    const dispatch = useDispatch();
+    const policeDepartments = useSelector(state => {
+        return state.pd.list.map(pdId => state.pd[pdId])
+    })
+    const singlePd = policeDepartments[pdId]
+
+    useEffect(() => {
+        dispatch(displayPds())
+    },[dispatch])
+
     return (
-        <h1>Single Police Department</h1>
+        <div>
+            <div>{singlePd?.department_name}</div>
+            <div>
+                <div>Department Address:</div>
+                <div>{singlePd?.street_address}</div>
+                <div>{singlePd?.city}, {singlePd?.state}, {singlePd?.zip_code}</div>
+            </div>
+            <div>
+                <h1>This is where the reflections go</h1>
+            </div>
+        </div>
     )
 }
 
